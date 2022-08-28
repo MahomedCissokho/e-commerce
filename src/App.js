edaee0753e6d54440b5b544be9187d85cd7a5749
Ciisco Mouhamed <mahomedcissokho@esp.sn>
2022-08-28 14:28:40 -0400

    updating our directory with components and categories

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import DirectoryComponent from "./components/directory-component/directory-component.container";
+
+const App = () => {
+    
+    return(
+          <DirectoryComponent />
+  )
+}
+
+export default App;
---
+++ src/components/category-item/category-item-component.jsx
@@ -0,0 +1,20 @@
+import './category-item-component.scss';
+
+const CategoryItem = ({category}) => {
+    const {imageUrl, title} = category ;
+    console.log(category);
+    return (
+        <div className = "category-container large">
+            <div className = 'background-image' style={{
+                backgroundImage :  `URL(${imageUrl})`
+            }}>
+            </div>
+            <div className = 'category-body-container'>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem ;
